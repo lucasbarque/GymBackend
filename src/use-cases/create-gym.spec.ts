db924dfab70f9e13e2fb6134671754b8f7b6c8cb
Lucas Barque <lucasbarquedasilva@hotmail.com>
2023-07-15 09:14:33 -0400

    Create Gym

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,24 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to register', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: null,
+      phone: null,
+      latitude: -20.4353613,
+      longitude: -54.6204637,
+    })
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
